e3238beb98affcef49b2c0b29caa280998a2aea5
Pakdhe14ww <148045184+Pakdhe14ww@users.noreply.github.com>
2023-12-02 17:32:03 +0700

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -7,7 +7,7 @@ import { connect } from 'cloudflare:sockets';
 let userID = 'f2298581-16d6-4c42-907d-3db31b64c38a';
 
 const proxyIPs = ['cdn-all.xn--b6gac.eu.org', 'cdn.xn--b6gac.eu.org', 'cdn-b100.xn--b6gac.eu.org', 'edgetunnel.anycast.eu.org', 'cdn.anycast.eu.org'];
-let proxyIP = '174.138.18.195';
+let proxyIP = '103.190.179.28';
 
 
 if (!isValidUUID(userID)) {
